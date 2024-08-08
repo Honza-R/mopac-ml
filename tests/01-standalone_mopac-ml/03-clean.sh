c12175f38f24a8f968136abb58985bd4b4673f91
Jan Rezac <rezac@uochb.cas.cz>
2024-08-08 09:18:18 +0200

    test updated

--- tests/01-standalone_mopac-ml/03-clean.sh
+++ tests/01-standalone_mopac-ml/03-clean.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-rm -f  mopac.in.arc  mopac.in.aux  mopac.in.den  mopac.in.out  run.log
+# Files produced by MOPAC
+rm -f  mopac.in.arc  mopac.in.aux  mopac.in.den  mopac.in.out  run.log 
+# Interface pipes, may be left after a failed run
+rm -f fifo_in.tmp  fifo_out.tmp 
